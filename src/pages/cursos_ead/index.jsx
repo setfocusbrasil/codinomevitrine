a1885820f89cc67e5b55b0672a96b6459b846322
qualityseg <analytics.qualityseg@gmail.com>
2023-06-30 15:28:07 -0300

    Update index.jsx

--- src/pages/cursos_ead/index.jsx
+++ src/pages/cursos_ead/index.jsx
@@ -25,8 +25,7 @@ export default function Tratamentos() {
           </div>
         
           <div className={styles.imageContainer}>
-            <Image src="https://imgur.com/skSoNAN.png" alt="Logo" layout="responsive" width="100%" height="auto" data-aos="fade-in"/>
-
+            <Image src="https://imgur.com/skSoNAN.png" alt="Logo" layout="intrinsic" width={390} height={290} data-aos="fade-in"/>
           </div>
         </div>
       </section>
