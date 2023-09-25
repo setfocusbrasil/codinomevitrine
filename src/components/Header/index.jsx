235a88e9eb3ec26a1aa1e87bb2cdf74a9f1ac94a
setfocusbrasil <119501939+setfocusbrasil@users.noreply.github.com>
2023-09-25 10:30:47 -0300

    116

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -75,7 +75,7 @@ export const Header = () => {
                 src="/icons/logo_transparente.png"
                 alt="QualitySeg Cursos EAD"
                 width="323"
-                height="96"
+                height="116"
               />
             </a>
           </Link>
