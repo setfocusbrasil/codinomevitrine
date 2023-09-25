466655e788713a7e2d48c06da253b0f2488b07f7
setfocusbrasil <119501939+setfocusbrasil@users.noreply.github.com>
2023-09-25 14:46:51 -0300

    bacon

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -144,8 +144,8 @@ const Catalogo = () => {
                   src={curso.imageSrc}
                   alt={curso.titulo}
                   className={styles.image}
-                  width={470}
-                  height={260}
+                  width={235}
+                  height={127}
                 />
               </div>
               <h3>{curso.titulo}</h3>
